a24b71a9dc99156f39d8e201d0e0d97f938ff165
Andrea <andr.accatino@gmail.com>
2020-03-06 14:59:47 +0100

    login screen def

--- scripts.js
+++ scripts.js
@@ -4,10 +4,14 @@ $("#login").click(function() {
   if (diventatoheader == true) {
     $(".box-logo").removeClass("box-logo-header");
     $(".netflix-logo").removeClass("netflix-logo-header");
+    $(".background-login").removeClass("background-login-full");
+    $(".login-form").removeClass("login-form-full");
     diventatoheader = false;
   } else {
     $(".box-logo").addClass("box-logo-header");
     $(".netflix-logo").addClass("netflix-logo-header");
+    $(".background-login").addClass("background-login-full");
+    $(".login-form").addClass("login-form-full");
     diventatoheader = true;
   }
 });
